e1e9741da2569cf7aecea32e7ba384a0bc02f350
Sujith G <sujith3g@gmail.com>
2015-06-28 16:12:22 +0530

    jsdoc documentation test

--- cordova/cordova_g_plus.js
+++ cordova/cordova_g_plus.js
@@ -1,4 +1,17 @@
+/**
+ * @summary The Meteor namespace
+ * @namespace Meteor
+ */
 Meteor.cordova_g_plus = function(request, callback) {
+  /**
+  * @function cordova_g_plus
+  * @summary function to call native google plus login on cordova apps.
+  * @memberof Meteor
+  * @param {object} request an object with gplus login details
+  * @param {Boolean} request.cordova_g_plus `request.cordova_g_plus` expected to be true to start native gplus login.
+  * @param {Array} request.profile Is an array of profile properties required, eg. `["email", "email_verified", "gender"]`
+  * @param {Function} callback `callback` function can have one argument `error` which will be containing the details of error if any.
+  */
     window.plugins.googleplus.login({},
         function(response) {
             request.email = response.email;
