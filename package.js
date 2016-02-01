94b73fa1d88edcfc9b3cbef0ddaa98b33644d12e
Linto Cheeran <linto.cet@gmail.com>
2016-02-02 04:19:27 +0530

    npm update

--- package.js
+++ package.js
@@ -1,41 +1,41 @@
 Package.describe({
-    name: "hedcet:cordova-google-plus-native-sign-in",
-    summary: "native SignIn with Google Plus in Meteor-Cordova Android/IOS Apps",
-    documentation: "README.md",
-    version: "1.0.1",
-    git: "https://github.com/sujith3g/meteor-cordova-google-plus.git"
+  name: "hedcet:cordova-google-plus-native-sign-in",
+  summary: "native SignIn with Google Plus in Meteor Cordova Android/IOS App",
+  documentation: "README.md",
+  version: "1.1.0",
+  git: "https://github.com/sujith3g/meteor-cordova-google-plus.git"
 });
 
 Cordova.depends({
-    "cordova-plugin-googleplus": "https://github.com/EddyVerbruggen/cordova-plugin-googleplus.git#03855ecdffecc0815adad7d6bf0824f62c190c08"
+  "cordova-plugin-googleplus": "https://github.com/EddyVerbruggen/cordova-plugin-googleplus.git#3ae99ea5d24619949613b87cdcb01b407199c253"
 });
 
 Package.onUse(function(api) {
-    api.versionsFrom("METEOR@1.0");
+  api.versionsFrom("METEOR@1.0");
 
-    api.use([
-        "accounts-base",
-        "check"
-    ], ["client", "server"]);
+  api.use([
+    "accounts-base",
+    "check"
+  ], ["client", "server"]);
 
-    api.use([
-        "http",
-        "underscore"
-    ], ["server"]);
+  api.use([
+    "http",
+    "underscore"
+  ], ["server"]);
 
-    api.imply(["accounts-base"], ["client", "server"]);
+  api.imply(["accounts-base"], ["client", "server"]);
 
-    api.add_files([
-        "server/cordova_g_plus.js"
-    ], ["server"]);
+  api.add_files([
+    "server/cordova_g_plus.js"
+  ], ["server"]);
 
-    api.add_files([
-        "cordova/cordova_g_plus.js"
-    ], ["web.cordova"]);
+  api.add_files([
+    "cordova/cordova_g_plus.js"
+  ], ["web.cordova"]);
 });
 
 Package.onTest(function(api) {
-    api.use(["hedcet:cordova-google-plus-native-sign-in", "tinytest"]);
+  api.use(["hedcet:cordova-google-plus-native-sign-in", "tinytest"]);
 
-    api.addFiles("tests/client/cordova_g_plus_test.js", "client");
+  api.addFiles("tests/client/cordova_g_plus_test.js", "client");
 });
